539c4f729918675c9af8ae574e14817a1493bc85
laje-E <136092273+laje-E@users.noreply.github.com>
2025-04-06 18:42:29 -0300

    Add files via upload

--- ejer_string_7.cpp
+++ ejer_string_7.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 int main () {
 	char palabra1 [20];
 	printf ("ingrese la primer palabra");
@@ -16,24 +17,33 @@ int main () {
 		int j = 0;
 		int resultado;
 		int igualdad;
+		char ordenado1 = std::sort(palabra1, palabra1);
+		char ordenado2 = std::sort(palabra2.begin(), palabra2.end());
+		/*
 		for (i=0;i<n;i++){
 			do{
 				resultado = palabra1[i] = palabra2[j];
 				if (resultado == 0){
 					igualdad = igualdad + 1;
-					/*
+					(
 					tengo que encontrar una forma para no comparar letras repetidas, por ejemplo con lolo y lona, no son iguales pero polo va tomar como que hay dos O iguales, cuando solo hay 1 O
 					yo estimo que una soluciòn es saltear una letra que ya se sabe que es igual, pero nose como
-					*/
+					)
 				}
 				j = j + 1;
 			}while(j<n || resultado != 0);
-		}
-		if (igualdad == n){
+		    
+    
+        }
+        */
+		if (strcmp (ordenado1, ordenado2) == 0){
 			printf ("las palabras son un anagrama");
 		}
-		else {
+		else if (strcmp (ordenado1, ordenado2) != 0) {
 			printf ("las palabras no son un anagrama");
 		}
+	}
+	else if (longitud1 != longitud2) {
+		printf ("las palabras no son un anagrama");
 	}
 }
